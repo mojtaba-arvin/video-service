49090e6adab6ba9ea5cedc46817d8f4cb7853496
Mojtaba Arvin <m.arvin@live.com>
2021-02-23 15:30:44 +0330

    fix service name

--- exec.codegen.sh
+++ exec.codegen.sh
@@ -3,4 +3,4 @@ docker-compose \
      -f docker-compose.video-streaming.yml \
      -f docker-compose.expose.yml \
      -f docker-compose.messaging.local.yml \
-     exec video-streaming /bin/sh -c "sh video-streaming/grpc/generate_grpc_codes.sh"
+     exec video-streaming /bin/sh -c "cd video-streaming/video_streaming/grpc/ sh ./generate_grpc_codes.sh"
--- video-streaming/video_streaming/grpc/generate_grpc_codes.sh
+++ video-streaming/video_streaming/grpc/generate_grpc_codes.sh
@@ -5,3 +5,5 @@ python -m grpc_tools.protoc -I./protos --python_out=./protos --grpc_python_out=.
 
 # to fix ModuleNotFoundError
 sed -i -e "s/import streaming_pb2/from . import streaming_pb2/g" ./protos/streaming_pb2_grpc.py
+
+# TODO : restart gRPC server
