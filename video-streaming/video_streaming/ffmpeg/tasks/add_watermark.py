b93a286087ba37a29e662d4464cf6e0b23bcdc87
Mojtaba Arvin <m.arvin@live.com>
2021-03-21 23:37:48 +0330

    ffmpeg watermark

--- video-streaming/video_streaming/ffmpeg/tasks/add_watermark.py
+++ video-streaming/video_streaming/ffmpeg/tasks/add_watermark.py
@@ -51,13 +51,93 @@ def add_watermark(
         request_id: str = None,
         output_id: str = None
         ) -> dict:
-    # TODO
-    return dict(
-        # when pass to create_playlist or generate_thumbnail task
+
+    # TODO add overlay params
+
+    self.check_add_watermark_requirements(
+        request_id=request_id,
+        output_id=output_id,
         video_path=video_path,
-        # when pass to upload_file task
-        file_path=video_path)
+        watermark_path=watermark_path,
+        output_path=output_path,
+        s3_output_key=s3_output_key)
 
-    # TODO add prefix 'watermarked_' when save on local
+    if self.is_forced_to_stop(request_id):
+        raise self.raise_revoke(request_id)
+    if self.is_output_forced_to_stop(request_id, output_id):
+        raise self.raise_revoke_output(request_id, output_id)
 
+    # save output status using output_id and request_id
+    self.save_output_status(
+        self.output_status.PREPARATION_PROCESSING,
+        output_id,
+        request_id)
 
+    # get output directory and set output_path if is None
+    output_path, directory = self.ensure_set_output_location(
+       request_id,
+       output_id,
+       output_path=output_path,
+       s3_output_key=s3_output_key)
+
+    # create directory with all parents, to prevent ffmpeg error
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    main = ffmpeg.input(video_path)
+    watermark = ffmpeg.input(watermark_path)
+
+    # TODO use callback
+    self.save_output_status(
+        self.output_status.PROCESSING,
+        output_id,
+        request_id)
+    try:
+        (ffmpeg.filter([main, watermark], 'overlay', 0, 0)
+            .output(output_path)
+            .run(
+                cmd=settings.FFMPEG_BIN_PATH,
+                capture_stdout=True,
+                capture_stderr=True,
+                # Overwrite output files without asking (ffmpeg -y option)
+                overwrite_output=True
+            )
+        )
+    except ffmpeg.Error as e:
+        raise self.retry(exc=e)
+
+    # process: Popen = (
+    #     ffmpeg.filter([main, watermark], 'overlay', 0, 0)
+    #     .output(output_path)
+    #     .run_async(
+    #         cmd=settings.FFMPEG_BIN_PATH,
+    #         pipe_stdout=True,
+    #         pipe_stderr=True,
+    #         # Overwrite output files without asking (ffmpeg -y option)
+    #         overwrite_output=True
+    #     ))
+    # callback: callable = FfmpegCallback(
+    #             task=self,
+    #             task_id=self.request.id.__str__(),
+    #             output_id=output_id,
+    #             request_id=request_id
+    #         ).ffmpeg_progress
+    # try:
+    #     ffmpeg_process = FfmpegProcess(
+    #         process=process,
+    #         progress_callback=callback
+    #     )
+    #     print("ffmpeg_process run")
+    #     ffmpeg_process.run()
+    # except ffmpeg.Error as e:
+    #     raise self.retry(exc=e)
+
+    self.save_output_status(
+        self.output_status.PROCESSING_FINISHED,
+        output_id,
+        request_id)
+
+    return dict(
+        # to use in create_playlist or generate_thumbnail task
+        video_path=output_path,
+        # to use in upload_file task
+        file_path=output_path)
